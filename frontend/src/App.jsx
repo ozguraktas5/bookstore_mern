380fc00351f47af17f62789302cfc16300adde36
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-04 19:53:30 +0300

    04.03.2024

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'; 
-import { CreateBook } from './pages/CreateBook';
-import { Home } from './pages/Home';
-import { ShowBook } from './pages/ShowBook';
-import { DeleteBook } from './pages/DeleteBook';
-import { EditBook } from './pages/EditBook';
+import CreateBook from './pages/CreateBook';
+import Home from './pages/Home';
+import ShowBook from './pages/ShowBook';
+import DeleteBook from './pages/DeleteBook';
+import EditBook from './pages/EditBook';
 
 const App = () => {
   return (
    <Routes>
-    <Route path='/' element={ <Home /> }></Route>
-    <Route path='/books/create' element={ <CreateBook /> }></Route>
-    <Route path='/books/details/:id' element={ <ShowBook />}></Route>
-    <Route path='/books/edit/:id' element={ <EditBook /> }></Route>
-    <Route path='/books/delete/:id' element={ <DeleteBook /> }></Route>
+    <Route path='/' element={<Home />}></Route>
+    <Route path='/books/create' element={<CreateBook />}></Route>
+    <Route path='/books/details/:id' element={<ShowBook />}></Route>
+    <Route path='/books/edit/:id' element={<EditBook />}></Route>
+    <Route path='/books/delete/:id' element={<DeleteBook />}></Route>
    </Routes>
   )
 }
