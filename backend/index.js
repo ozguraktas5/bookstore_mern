39036c11b06cc345d11a19a34b63fb12d324c5a5
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-07 20:03:18 +0300

    07.03.2024

--- backend/index.js
+++ backend/index.js
@@ -15,15 +15,9 @@ app.use(
     origin: "https://bookstore-mern-api.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     
+    
   })
 );
-
-app.use(function (request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 
 app.get("/", (request, response) => {
   console.log(request);
