a2a6594a94982c896b28f5af35f1ca62a3d6016c
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-07 19:50:28 +0300

    07.03.2024

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,13 @@ app.use(cors());
 
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: "https://bookstore-mern-api.vercel.app/",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"],
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://bookstore-mern-api.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
