fb76eecd5f9dec83eaf14bdfad98f1fc232c911f
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-07 20:19:17 +0300

    07.03.2024

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,24 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
-
+// Middleware for parsing request body
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "https://bookstore-mern-api.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send("server running");
+  return response.status(234).send("Welcome To MERN Stack Tutorial");
 });
 
 app.use("/books", booksRoute);
