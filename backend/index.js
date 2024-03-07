4f9ba5d237fb8c7a9a576b66202543f39b8cfe67
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-07 18:51:28 +0300

    07.03.2024

--- backend/index.js
+++ backend/index.js
@@ -2,28 +2,26 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://bookstore-mern-server-lilac.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-
-app.use(cors());
-
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET','POST','PUT','DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// )
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN Stack");
 });
 
-app.use('/books', booksRoute);
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
