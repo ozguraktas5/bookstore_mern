ea7f2a000e33397a065113e1d7aa66be5fe4062b
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-09 21:04:24 +0300

    09.03.2024

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 // app.use(
 //   cors({
-//     origin: 'http://localhost:5555',
+//     origin: 'http://localhost:3000',
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //     allowedHeaders: ['Content-Type'],
 //   })
