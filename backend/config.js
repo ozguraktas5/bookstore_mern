f012d3efb3e52dba55c882015a73ac12982bb724
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-09 20:32:24 +0300

    09.03.2024

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const PORT = 5555;
+export const PORT = 5173;
 export const mongoDBURL = "mongodb+srv://bookstore_mern:bookstore_mern123@cluster0.1thcrlq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
